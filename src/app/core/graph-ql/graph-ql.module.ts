48a05b28ddaa1ab041ce0d1c393df8154fa1152c
Carlos Nava <machinery.e@gmail.com>
2022-04-05 15:14:06 -0400

    Instalar apolo

---
+++ src/app/core/graph-ql/graph-ql.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { GraphQLRoutingModule } from './graph-ql-routing.module';
+
+
+import {HttpClientModule} from '@angular/common/http';
+import {ApolloModule, APOLLO_OPTIONS} from 'apollo-angular';
+import {HttpLink} from 'apollo-angular/http';
+import {InMemoryCache} from '@apollo/client/core';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    GraphQLRoutingModule,
+    ApolloModule,
+    HttpClientModule
+  ],
+  providers: [
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory: (httpLink: HttpLink) => {
+        return {
+          cache: new InMemoryCache(),
+          link: httpLink.create({
+            uri: 'https://48p1r2roz4.sse.codesandbox.io',
+          }),
+        };
+      },
+      deps: [HttpLink],
+    },
+  ],
+})
+export class GraphQLModule { }
